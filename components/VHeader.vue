073322fe1aa9159319aea6de612fc630dd4387b9
mbank14 <jembar.ashofa01@gmail.com>
2021-09-18 03:46:17 +0700

    nyicil duls

--- components/VHeader.vue
+++ components/VHeader.vue
@@ -1,22 +1,79 @@
 <template>
   <div>
     <!-- navigasi -->
-    <nav></nav>
+    <nav>
+        <!-- logo -->
+        <div>
+            <img src="" alt="logo elemes.id">
+        </div>
+
+        <!-- navigation -->
+        <div>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Promotion <span>HOT</span></a>
+            <a href="">Blogs</a>
+            <a href="">Contact Us</a>
+        </div>
+
+        <!-- button register and login -->
+        <div>
+            <button-base :buttonText="'Daftar Sekarang'"></button-base>
+        </div>
+    </nav>
 
     <!-- hero -->
-    <div></div>
+    <div>
+        <!-- Motto / Jargon -->
+        <div></div>
+
+        <!-- images -->
+        <div></div>
+
+        <!-- description and CTA -->
+        <div>
+            <!-- description -->
+            <p></p>
+
+            <!-- CTA -->
+            <div>   
+                <button-base :buttonText="'Daftar Sekarang'"></button-base>
+                <button-base :buttonText="'About Us'"></button-base>
+            </div>
+            
+        </div>
+    </div>
 
 
 
     <!-- navigasi mobile -->
     <div>
-        
+        <div>
+            <!-- logo -->
+            <a href=""></a>
+
+            <a href="">Home</a>
+        </div>
+        <div>
+            <!-- logo -->
+            <a href=""></a>
+
+            <a href="">Promotions</a>
+        </div>
+        <div>
+            <!-- logo -->
+            <a href=""></a>
+
+            <a href="">Others</a>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
+import ButtonBase from './elements/ButtonBase.vue'
 export default {
+  components: { ButtonBase },
 
 }
 </script>
